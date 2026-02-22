bc08aa3c66d700e29adfdc90a58cb61131521b91
ian <ian@BASHA>
2026-02-22 14:55:04 +0800

    Add project files.

---
+++ Santiago_BSIT-WM-202_Lesson2Activity/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Santiago_BSIT-WM-202_Lesson2Activity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Santiago_BSIT-WM-202_Lesson2Activity")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be78b79c-93c0-48ff-a046-6718453e5ed1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Santiago_BSIT-WM-202_Lesson2Activity/Santiago_BSIT-WM-202_Lesson2Activity.cs
@@ -0,0 +1,58 @@
+﻿Question 2:Browse Button
+code:
+
+    private void btnBrowse_Click(object sender, EventArgs e)
+{
+    OpenFileDialog ofd = new OpenFileDialog();
+    ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+    if (ofd.ShowDialog() == DialogResult.OK)
+    {
+        pictureBox1.Image = Image.FromFile(ofd.FileName);
+    }
+}
+
+Question 3: Insert Data into ComboBox
+code:
+
+    private void Form1_Load(object sender, EventArgs e)
+{
+    cboPrograms.Items.Add("BS Computer Engineering");
+    cboPrograms.Items.Add("BS Civil Engineering");
+    cboPrograms.Items.Add("BS Electrical Engineering");
+    cboPrograms.Items.Add("BS Electronics Engineering");
+    cboPrograms.Items.Add("BS Mechanical Engineering");
+}
+
+Question 4: Clear All TextBoxes
+code:
+
+    private void btnNewCancel_Click(object sender, EventArgs e)
+{
+    txtStudentName.Text = "";
+    txtStudentNumber.Text = "";
+    txtYearLevel.Text = "";
+    txtScholar.Text = "";
+    txtCourseNumber.Text = "";
+    txtCourseCode.Text = "";
+    txtCourseDesc.Text = "";
+    txtUnitLecture.Text = "";
+    txtUnitLaboratory.Text = "";
+    txtTime.Text = "";
+    txtDay.Text = "";
+    cboPrograms.SelectedIndex = -1; //adds tet boxex if needed
+    pictureBox1.Image = null;
+}
+
+Question 5: Transfer TextBox Data to ListBoxes/TextBoxes
+code:
+
+    private void btnSubmit_Click(object sender, EventArgs e)
+{
+    lstCourses.Items.Add(txtCourseNumber.Text); //adds course info
+
+    txtTotalTuitionFee.Text = txtCourseTuitionFee.Text;  //transfers total to sum boxess
+    txtTotalMiscFee.Text = txtCourseMiscFee.Text;
+    txtTotalUnits.Text = txtCreditUnits.Text;
+    txtTotalTuitionAndFees.Text = txtCourseTotalTuitionAndFee.Text;
+}
